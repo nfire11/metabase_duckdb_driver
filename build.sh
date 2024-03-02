97ee98b50ec2693a0829a449655247f3cd62ad35
Evan W <nfire11@gmail.com>
2024-03-02 17:23:11 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $(dirname $0) 
+DRIVER_PATH=$(pwd)
+
+cd ../metabase
+
+clojure \
+  -Sdeps '{ :aliases {:duckdb { :extra-deps {org.duckdb/duckdb_jdbc {:mvn/version "0.10.0"}} }}}'  \
+  -X:build:duckdb \
+  build-drivers.build-driver/build-driver! \
+  "{:driver :duckdb, :project-dir \"${DRIVER_PATH}\", :target-dir \"${DRIVER_PATH}/target\"}"
+
+cd "${DRIVER_PATH}"
+
