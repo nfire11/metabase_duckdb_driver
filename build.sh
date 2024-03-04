f3ae1be899f18347caaf4e1ec69d774a642e100f
Evan W <nfire11@gmail.com>
2024-03-04 15:38:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,14 @@ DRIVER_PATH=$(pwd)
 cd ../metabase
 
 clojure \
-  -Sdeps '{ :aliases {:duckdb { :extra-deps {org.duckdb/duckdb_jdbc {:mvn/version "0.10.0"}} com.metabase/duckdb-driver {:local/root "'${DRIVER_PATH}'"} {metabase/metabase-core {:local/root "../metabase"}
-                metabase/build-drivers {:local/root "../metabase/bin/build-drivers"}} }}}'  \
+  -Sdeps '{:aliases
+ {:build
+  {:extra-deps {metabase/metabase-core {:local/root "../metabase"}
+                metabase/build-drivers {:local/root "../metabase/bin/build-drivers"}}
+   :exec-fn    build-drivers.build-driver/build-driver!
+   :exec-args  {:driver      :duckdb
+                :project-dir "."
+                :target-dir  "./target"}}}}'  \
   -X:build:duckdb \
   build-drivers.build-driver/build-driver! \
   "{:driver :duckdb, :project-dir \"${DRIVER_PATH}\", :target-dir \"${DRIVER_PATH}/target\"}"
